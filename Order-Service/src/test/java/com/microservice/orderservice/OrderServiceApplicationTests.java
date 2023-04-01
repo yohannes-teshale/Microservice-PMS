4726f108820ffb95a7e161ef8e381185dfc5a060
koeda <yohannesteshle@gmail.com>
2023-04-01 12:23:23 -0500

    final touch to orders service

--- Order-Service/src/test/java/com/microservice/orderservice/OrderServiceApplicationTests.java
+++ Order-Service/src/test/java/com/microservice/orderservice/OrderServiceApplicationTests.java
@@ -2,12 +2,24 @@ package com.microservice.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class OrderServiceApplicationTests {
+
+    @Container
+    static MySQLContainer mySQLContainer= new MySQLContainer("mysql:8.0");
+
 
     @Test
     void contextLoads() {
+
+
+
     }
 
 }
